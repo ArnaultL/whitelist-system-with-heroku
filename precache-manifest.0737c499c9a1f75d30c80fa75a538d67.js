5fafc622861a0b18fe06222757a0d41ea40288c0
Arnault <arnault.lalloz@gmail.com>
2022-04-22 00:31:27 +0200

    Updates

---
+++ precache-manifest.0737c499c9a1f75d30c80fa75a538d67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcef814a97695723b24698700375ee0c",
+    "url": "/whitelist-system-with-heroku/index.html"
+  },
+  {
+    "revision": "78bc7084455aaf1d57d1",
+    "url": "/whitelist-system-with-heroku/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "74719ab09a50d621d15a",
+    "url": "/whitelist-system-with-heroku/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "78bc7084455aaf1d57d1",
+    "url": "/whitelist-system-with-heroku/static/js/2.40af56dc.chunk.js"
+  },
+  {
+    "revision": "74719ab09a50d621d15a",
+    "url": "/whitelist-system-with-heroku/static/js/main.6fba4754.chunk.js"
+  },
+  {
+    "revision": "8f9b97b3a0aea1617ba3",
+    "url": "/whitelist-system-with-heroku/static/js/runtime-main.0c3d8abb.js"
+  }
+]);
